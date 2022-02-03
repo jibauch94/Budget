7324858bea3eefe07d935421bcfc7cddfbe58299
Jonas Ishøj Bauch <jib090994@gmail.com>
2022-02-03 16:01:35 +0100

    added income table

---
+++ app/Http/Controllers/IncomeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Income;
+use App\Http\Requests\StoreIncomeRequest;
+use App\Http\Requests\UpdateIncomeRequest;
+
+class IncomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreIncomeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreIncomeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateIncomeRequest  $request
+     * @param  \App\Models\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateIncomeRequest $request, Income $income)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Income  $income
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Income $income)
+    {
+        //
+    }
+}
