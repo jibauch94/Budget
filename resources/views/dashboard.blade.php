56f50b9dd02c11c1849d094e02b851abd7bc1467
Jonas Ishøj Bauch <jib090994@gmail.com>
2022-02-10 09:21:26 +0100

    added my own grid to dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,4 +12,36 @@
             </div>
         </div>
     </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                <a href=""> Opret budget </a>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-500">
+                                Her kan du oprette dit budget. Du vil ved oprettelse blive bedt om at indtaste diverse indtægter og udgifter. Du kan altid ændre i dit budget løbende, så du altid har det korrekte budget ved hånden.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                <a href=""> Opret budget </a>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-500">
+                                Her kan du få hele overblikket over dit budget. Indtægter, udgifter, restbeløb til måneden osv. Derudover vil du kunne se hvad dine penge bliver brugt på.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
